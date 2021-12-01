d7bb508c7afca4214a3af1acd6df15424117112c
kieuhuy <kieuphuchuy2002@gmail.com>
2021-12-01 12:06:26 +0700

    react-lab14

--- Redux-demo/redux-toolkit/db/product.js
+++ Redux-demo/redux-toolkit/db/product.js
@@ -1,5 +1,5 @@
 const products = [
-    { id: 1, name: "Product 1", price: 10 },
+    { id: 1, title: "Product 1", price: 10 },
     { id: 2, name: "Product 2", price: 5 },
     { id: 3, name: "Product 3", price: 2 },
     { id: 4, name: "Product 4", price: 8 },
